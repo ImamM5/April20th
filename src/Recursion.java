88883fbabf4e2bd8681ebce023419835bcd686fd
ImamM5 <imamm@nycstudents.net>
2023-04-24 08:27:31 -0400

    new code

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,8 +1,16 @@
 public class Recursion
 {
     public static void main(String [] args){
+        System.out.println(lcm(10, 15));
+        System.out.println(lcm(2, 8));
+        System.out.println(lcm(7, 5));
         System.out.println(gcd(10, 15));
+        System.out.println(gcd(81, 27));
+        System.out.println(gcd(20, 45));
+        System.out.println(toBinary(5));
+        System.out.println(toBinary(7));
     }
+
     public static int gcd(int a, int b)
     {
         if (b == 0)
@@ -14,4 +22,25 @@ public class Recursion
             return gcd(b, a % b);
         }
     }
+
+    public static int lcm(int a, int b)
+    {
+        return Math.abs(a*b)/gcd(a,b);
+    }
+
+    public static String toBinary(int n)
+    {
+        if (n==0)
+        {
+            return "";
+        }
+        else
+        {
+            return toBinary(n/2)+ (n%2);
+        }
+    }
+
+
+
 }
+
